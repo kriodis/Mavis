1665ea5835453a899032aa817b58d461046e0cbf
Franco Scolari <kriodis@hotmail.com>
2018-11-13 17:39:14 -0300

    changes

--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -1,24 +1,28 @@
-import { UserService } from './../../services/user.service';
-import { UserModel } from './../../models/userModel.model';
-import { Component } from '@angular/core';
+import { UserService } from "./../../services/user.service";
+import { UserModel } from "./../../models/userModel.model";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: 'contact.page.html',
-  styleUrls: ['contact.page.scss']
+  selector: "app-contact",
+  templateUrl: "contact.page.html",
+  styleUrls: ["contact.page.scss"]
 })
 export class ContactPage {
-  contacts;
+
+  contacts: UserModel[] = [
+    {
+      id: "1",
+      nick: "fran",
+      name: "fran",
+      status: "programando"
+    }
+  ];
+
   constructor(private userService: UserService) {
-    this.contacts = this.userService.getUser();
-    console.log(this.contacts);
+    // this.contacts = this.userService.getUser();
+    // console.log(this.contacts);
   }
 
-  delete(contact) {
-
-  }
-  chat(contact) {
-
-  }
-
+  delete(contact) {}
+  chat(contact) {}
 }
