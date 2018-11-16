b3276ef3da36f04ddddd7aa9584ce66c541be520
Franco Scolari <kriodis@hotmail.com>
2018-11-16 15:59:07 -0300

    add  chat's style

--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -1,32 +1,52 @@
-import { UserModel } from './../../models/userModel.model';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { UserService } from '../../services/user.service';
+import { UserModel } from "./../../models/userModel.model";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { UserService } from "../../services/user.service";
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.page.html',
-  styleUrls: ['./chat.page.scss'],
+  selector: "app-chat",
+  templateUrl: "./chat.page.html",
+  styleUrls: ["./chat.page.scss"]
 })
 export class ChatPage implements OnInit {
   userId: any;
   contact: UserModel;
+  currentChat: any[];
+  message: string;
+
   constructor(
     private activatedRoute: ActivatedRoute,
-    private userService: UserService) {
-    this.userId = this.activatedRoute.snapshot.params['id'];
-    this.userService.getUserById(this.userId).valueChanges().subscribe(
-      (data: UserModel) => {
-        this.contact = data;
-      },
-      (error) => {
-        console.log(error);
-
-      })
+    private userService: UserService
+  ) {
+    this.userId = this.activatedRoute.snapshot.params["id"];
+    this.userService
+      .getUserById(this.userId)
+      .valueChanges()
+      .subscribe(
+        (data: UserModel) => {
+          this.contact = data;
+        },
+        error => {
+          console.log(error);
+        }
+      );
   }
 
   ngOnInit() {
-    console.log(this.contact);
+    this.currentChat = [
+      { user: "user", message: "hey, whaasdaaaaaaa aaaaaaaaaaaaaaaaawdawdawdawdadwat are you doing?" },
+      { user: "currentUser", message: "learnisdfsdfsdfsdfsdfsdffsd fsdfsdfng angular 4" },
+      { user: "user", message: "cool is great" },
+      { user: "currentUser", message: "i know that's right" },
+      { user: "user", message: "hey, what are you doing?" },
+      { user: "currentUser", message: "learnisdfsdfsasdasdsadsadadfsdfsdfsdffsdfsdfsdfng angular 4" },
+      { user: "user", message: "cool is greasdat" },
+      { user: "currentUser", message: "i know that's right" }
+    ];
   }
 
+  chat() {
+    const newMessage = Object.assign(this.message, { user: 'anonymous'});
+    this.currentChat.push(newMessage);
+  }
 }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,24 +1,35 @@
-import { UserService } from './../../services/user.service';
-import { Component, OnInit } from '@angular/core';
+import { UserService } from "./../../services/user.service";
+import { Component, OnInit } from "@angular/core";
+import { ToastController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"]
 })
 export class LoginPage implements OnInit {
+  router: any;
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    public toastCtrl: ToastController
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async presentToast(error) {
+    const toast = await this.toastCtrl.create({
+      message: "Error: " + error + ". Retrying...",
+      duration: 3000,
+      position: "top"
+    });
+    toast.present();
   }
 
   loginWithFacebook() {
-    this.userService.facebookLogin().then(
-      (data) => {
-        console.log(data);
-      }).catch(
-        (error) => console.log(error)
-      )
+    this.userService
+      .facebookLogin()
+      .then(this.router.navigate(["tabs"]))
+      .catch(error => this.presentToast(error));
   }
 }
